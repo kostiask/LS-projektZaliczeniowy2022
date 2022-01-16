33c95cc9404d0e4dfe880221cd55ae8bb09470de
Mateusz Ostapiuk <ostek@vp.pl>
2022-01-16 21:03:51 +0100

    PS-11 Code review

--- src/main/java/pl/kul/operations/Delete.java
+++ src/main/java/pl/kul/operations/Delete.java
@@ -9,7 +9,31 @@ import static pl.kul.operations.validation.OperationValidatoError.INVALID_VALUE;
 public class Delete extends Operation{
 
 
-    public Delete(String operacja, Shop shop, String description, int[] numberOfArguments) {
-        super(operacja, shop, description, numberOfArguments);
+    public Delete(Shop shop) {
+        super("DELETE", shop, "'all' lub [id] - Usuwa wszystkie lub wybrany produkt", new int[]{1});
+    }
+
+    @Override
+    public String execute(String ... arg) {
+        if(arg[0].equals("all")){
+            this.getShop().removeAllProducts();
+            return "Zostaly usuniete wszystkie produkty ze sklepu.";
+        }
+        else {
+            if(Operation.isParsableInt(arg[0])){
+                int id = Integer.parseInt(arg[0]);
+                for(int i = 0; i < this.getShop().howManyProducts(); i ++){
+                    if(this.getShop().getProduct(i).getId() == id){
+                        String answer =  "Zostal usuniety: " + this.getShop().getProduct(i);
+                        this.getShop().removeProduct(i);
+                        return answer;
+                    }
+                }
+                throw  new OperationValidationException(INVALID_ID.getMessage());
+            }
+            else {
+                throw new OperationValidationException(INVALID_VALUE.getMessage());
+            }
+        }
     }
 }
